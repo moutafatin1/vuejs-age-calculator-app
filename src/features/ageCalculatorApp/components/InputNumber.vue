47b5432e55547617395864fe84ddf5254f7bddcc
Oussama Moutafatin <moutafatin3@gmail.com>
2023-04-05 08:05:57 +0000

    a working app

---
+++ src/features/ageCalculatorApp/components/InputNumber.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+import { useField } from 'vee-validate'
+
+const props = defineProps<{
+  name: string
+}>()
+
+const { errorMessage, value } = useField(() => props.name)
+</script>
+
+<template>
+  <div class="relative flex flex-col gap-1">
+    <label class="flex flex-col gap-1">
+      <span
+        class="text-sm uppercase text-smokeGrey"
+        :class="{ 'text-primary-lightRed': errorMessage }"
+        >{{ name }}</span
+      >
+      <input
+        type="number"
+        v-model="value"
+        class="h-14 w-20 rounded-lg border-lightGrey focus:border-primary-purple focus:ring-primary-purple md:w-24 md:text-xl lg:w-28"
+        :class="{ 'border-primary-lightRed': errorMessage }"
+      />
+    </label>
+    <p
+      v-if="errorMessage"
+      class="absolute top-full mt-1 font-sans text-sm font-normal italic text-primary-lightRed"
+    >
+      {{ errorMessage }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type='number'] {
+  -moz-appearance: textfield;
+  appearance: textfield;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,71 +1,71 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import InputNumber from '@/features/ageCalculatorApp/components/InputNumber.vue';
+import { toTypedSchema } from '@vee-validate/zod';
+import dayjs from "dayjs";
+import { useForm } from 'vee-validate';
+import { ref } from 'vue';
+import { z } from 'zod';
+
+const schema = z.object({
+  day: z.coerce.number().min(1, 'Must be a valid day').max(31, 'Must be a valid day'),
+  month: z.coerce.number().min(1, 'Must be a valid month').max(12, 'Must be a valid month'),
+  year: z.coerce
+    .number()
+    .min(1, 'Must be a valid year')
+    .max(new Date().getFullYear(), 'Must be in the past')
+})
+
+const { handleSubmit, handleReset } = useForm({
+  validationSchema: toTypedSchema(schema)
+})
+const age = ref<{
+  years: number | null,
+  months: number | null,
+  days: number | null
+}>()
+
+const onSubmit = handleSubmit((data) => {
+  const birthDate = dayjs(`${data.year}-${data.month}-${data.day}`)
+  const currentDate = dayjs(Date.now())
+  const years = currentDate.diff(birthDate, "year")
+  const months = currentDate.diff(birthDate, "month") - years * 12
+  const days = currentDate.diff(birthDate.add(years, "year").add(months, "month"), "day")
+
+  age.value = { years, months, days }
+  // handleReset()
+
+})
+</script>
 
 <template>
   <main class="flex h-screen items-start justify-center bg-lightGrey font-poppins md:items-center">
-    <div
-      class="custom-radius mx-auto mt-16 w-full max-w-xs bg-white px-4 py-12 md:max-w-lg md:px-8 lg:max-w-xl"
-    >
-      <form class="flex items-center justify-between gap-4 md:justify-start md:gap-8">
-        <label class="flex flex-col gap-1">
-          <span class="text-sm uppercase text-smokeGrey">day</span>
-          <input
-            type="number"
-            placeholder="DD"
-            class="h-14 w-20 rounded-lg border-lightGrey focus:border-primary-purple focus:ring-primary-purple md:w-24 md:text-xl"
-          />
-        </label>
-        <label class="flex flex-col gap-1">
-          <span class="text-sm uppercase text-smokeGrey">month</span>
-          <input
-            type="number"
-            placeholder="MM"
-            class="h-14 w-20 rounded-lg border-lightGrey focus:border-primary-purple focus:ring-primary-purple md:w-24 md:text-xl"
-          />
-        </label>
-        <label class="flex flex-col gap-1">
-          <span class="text-sm uppercase text-smokeGrey">year</span>
-          <input
-            type="number"
-            placeholder="YYYY"
-            class="h-14 w-20 rounded-lg border-lightGrey focus:border-primary-purple focus:ring-primary-purple md:w-24 md:text-xl"
-          />
-        </label>
+    <div class="custom-radius mx-auto mt-16 w-full max-w-xs bg-white px-4 py-12 md:max-w-lg md:px-8 lg:max-w-xl">
+      <form id="age-form" class="flex items-center justify-between gap-4 md:justify-start md:gap-8" @submit="onSubmit">
+        <InputNumber name="day" />
+        <InputNumber name="month" />
+        <InputNumber name="year" />
       </form>
-      <div class="my-14 flex items-center justify-center md:my-0">
+      <div class="my-14 flex items-center justify-center md:my-0 md:mt-4">
         <hr class="w-full max-w-xs md:max-w-none" />
-        <button
-          class="absolute z-10 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-primary-purple transition hover:bg-offBlack active:scale-95 md:static md:h-20 md:w-20 md:shrink-0"
-        >
+        <button form="age-form"
+          class="absolute z-10 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-primary-purple transition hover:bg-offBlack active:scale-95 md:static md:h-20 md:w-20 md:shrink-0">
           <svg xmlns="http://www.w3.org/2000/svg" width="35" height="40" viewBox="0 0 46 44">
             <g fill="none" stroke="#FFF" stroke-width="2">
-              <path
-                d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44"
-              />
+              <path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44" />
             </g>
           </svg>
         </button>
       </div>
       <div class="text-4xl font-extrabold italic md:text-7xl">
-        <p><span class="text-primary-purple">- -</span> years</p>
-        <p><span class="text-primary-purple">- -</span> months</p>
-        <p><span class="text-primary-purple">- -</span> days</p>
+        <p><span class="text-primary-purple">{{ age?.years ?? "- -" }}</span> years</p>
+        <p><span class="text-primary-purple">{{ age?.months ?? "- -" }}</span> months</p>
+        <p><span class="text-primary-purple">{{ age?.days ?? "- -" }}</span> days</p>
       </div>
     </div>
   </main>
 </template>
 
 <style scoped>
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
-
-/* Firefox */
-input[type='number'] {
-  -moz-appearance: textfield;
-  appearance: textfield;
-}
 .custom-radius {
   border-radius: 25px 25px 100px 25px;
 }
