965d876ac796a33700abb7c017b43db622971146
Oussama Moutafatin <moutafatin3@gmail.com>
2023-04-05 05:52:34 +0000

    style guide

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,7 @@
-<script setup lang="ts">
-import { MyButton } from '@/components/elements'
-import InputField from '@/components/forms/InputField.vue'
-import { toTypedSchema } from '@vee-validate/zod'
-import { useForm } from 'vee-validate'
-import { z } from 'zod'
-const formSchema = z.object({
-  username: z.string().min(1, 'Username is required')
-})
-const { handleSubmit } = useForm({
-  validationSchema: toTypedSchema(formSchema)
-})
-const onSubmit = handleSubmit((data) => {
-  console.log(data)
-})
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <h1 class="mt-8 text-center text-5xl text-violet-600">Home Page</h1>
-    <form @submit="onSubmit" class="mx-auto mt-8 max-w-xl">
-      <InputField name="username" placeholder="Username" />
-      <MyButton class="mt-2 w-full">Submit</MyButton>
-    </form>
+    <h1 class="mt-8 text-center font-poppins text-5xl text-primary-lightRed">Home Page</h1>
   </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: {
+          purple: 'hsl(259, 100%, 65%)',
+          lightRed: 'hsl(0, 100%, 67%)'
+        },
+        offWhite: 'hsl(0, 0%, 94%)',
+        lightGrey: 'hsl(0, 0%, 86%)',
+        smokeGrey: 'hsl(0, 1%, 44%)',
+        offBlack: 'hsl(0, 0%, 8%)'
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
+      }
+    }
   },
   plugins: [require('@tailwindcss/forms')]
 }
