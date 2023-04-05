e1ee87a670773650c80c08407373d26f0d6c4572
Oussama Moutafatin <moutafatin3@gmail.com>
2023-04-05 06:40:44 +0000

    fix sizes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main class="flex h-screen items-start justify-center bg-lightGrey font-poppins">
+  <main class="flex h-screen items-start justify-center bg-lightGrey font-poppins md:items-center">
     <div
-      class="custom-radius mx-auto mt-16 w-full max-w-xs bg-white px-4 py-12 md:max-w-md md:px-8 lg:max-w-lg"
+      class="custom-radius mx-auto mt-16 w-full max-w-xs bg-white px-4 py-12 md:max-w-lg md:px-8 lg:max-w-xl"
     >
       <form class="flex items-center justify-between gap-4 md:justify-start md:gap-8">
         <label class="flex flex-col gap-1">
@@ -11,7 +11,7 @@
           <input
             type="number"
             placeholder="DD"
-            class="w-20 rounded-lg border-lightGrey focus:border-primary-purple focus:ring-primary-purple md:w-24 md:text-xl"
+            class="h-14 w-20 rounded-lg border-lightGrey focus:border-primary-purple focus:ring-primary-purple md:w-24 md:text-xl"
           />
         </label>
         <label class="flex flex-col gap-1">
@@ -19,7 +19,7 @@
           <input
             type="number"
             placeholder="MM"
-            class="w-20 rounded-lg border-lightGrey focus:border-primary-purple focus:ring-primary-purple md:w-24 md:text-xl"
+            class="h-14 w-20 rounded-lg border-lightGrey focus:border-primary-purple focus:ring-primary-purple md:w-24 md:text-xl"
           />
         </label>
         <label class="flex flex-col gap-1">
@@ -27,14 +27,14 @@
           <input
             type="number"
             placeholder="YYYY"
-            class="w-20 rounded-lg border-lightGrey focus:border-primary-purple focus:ring-primary-purple md:w-24 md:text-xl"
+            class="h-14 w-20 rounded-lg border-lightGrey focus:border-primary-purple focus:ring-primary-purple md:w-24 md:text-xl"
           />
         </label>
       </form>
       <div class="my-14 flex items-center justify-center md:my-0">
         <hr class="w-full max-w-xs md:max-w-none" />
         <button
-          class="absolute z-10 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-primary-purple transition hover:bg-offBlack active:scale-95 md:static md:shrink-0"
+          class="absolute z-10 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full bg-primary-purple transition hover:bg-offBlack active:scale-95 md:static md:h-20 md:w-20 md:shrink-0"
         >
           <svg xmlns="http://www.w3.org/2000/svg" width="35" height="40" viewBox="0 0 46 44">
             <g fill="none" stroke="#FFF" stroke-width="2">
@@ -45,7 +45,7 @@
           </svg>
         </button>
       </div>
-      <div class="text-4xl font-extrabold italic md:text-5xl">
+      <div class="text-4xl font-extrabold italic md:text-7xl">
         <p><span class="text-primary-purple">- -</span> years</p>
         <p><span class="text-primary-purple">- -</span> months</p>
         <p><span class="text-primary-purple">- -</span> days</p>
